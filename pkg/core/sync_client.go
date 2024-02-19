c9ad8c7d1687df9649c262ed93ebdae26324ada5
Mykhailo Kokolius <mykhailo.kokolius@fti.de>
2024-02-19 15:49:35 +0100

    implement all request for the handshake

--- pkg/core/sync_client.go
+++ pkg/core/sync_client.go
@@ -21,44 +21,11 @@ func HandleHandShake(config domain.Conf) {
 		os.Exit(1)
 	}
 
-	sendPingRequest(connClient)
-	sendReplicationRequest(connClient, config)
-
-	err = connClient.Close()
-
-	if err != nil {
-		fmt.Println("Can't close the connection:", err.Error())
-
-		os.Exit(1)
-	}
-}
-
-func sendPingRequest(connClient net.Conn) {
-	_, err := connClient.Write([]byte("*1\r\n$4\r\nping\r\n"))
-
-	if err != nil {
-		fmt.Println("Write to server failed:", err.Error())
-
-		os.Exit(1)
-	}
-
-	reply := make([]byte, 1024)
-
-	_, err = connClient.Read(reply)
-
-	if err != nil {
-		fmt.Println("Write to server failed:", err.Error())
-
-		os.Exit(1)
-	}
-
-	fmt.Println("reply from server=", string(reply))
-}
-
-func sendReplicationRequest(connClient net.Conn, config domain.Conf) {
 	commands := []string{
+		"*1\r\n$4\r\nping\r\n",
 		fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n%d\r\n", config.OpenPort),
 		"*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n",
+		"*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n",
 	}
 
 	for _, command := range commands {
@@ -83,5 +50,13 @@ func sendReplicationRequest(connClient net.Conn, config domain.Conf) {
 		}
 
 		fmt.Println("reply from server=", string(reply))
+	}
+
+	err = connClient.Close()
+
+	if err != nil {
+		fmt.Println("Can't close the connection:", err.Error())
+
+		os.Exit(1)
 	}
 }
