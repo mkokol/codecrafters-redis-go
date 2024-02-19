322c1fca979016a4310b3b5a87cf7d9540a86922
Mykhailo Kokolius <mykhailo.kokolius@fti.de>
2024-02-19 13:25:17 +0100

    add ping request for replica handshake

--- app/server.go
+++ app/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"math/rand"
 	"net"
@@ -13,22 +12,17 @@ import (
 )
 
 type Conf struct {
-	openPort  int
-	replicaOf string
+	openPort   int
+	masterHost string
+	masterPort int
 }
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	openPortParam := flag.Int("port", 6379, "Port on which application would be lunched.")
-	replicaOfParam := flag.String("replicaof", "", "Host and Port of the master server.")
-	flag.Parse()
 
-	c := Conf{
-		openPort:  *openPortParam,
-		replicaOf: *replicaOfParam,
-	}
-
-	listener, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(c.openPort))
+	config := parseCliParams()
+	listener, err := net.Listen("tcp", "0.0.0.0:"+strconv.Itoa(config.openPort))
+	go handleHandShake(config)
 
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -46,7 +40,98 @@ func main() {
 			continue
 		}
 
-		go handleClient(conn, dict, c)
+		go handleClient(conn, dict, config)
+	}
+}
+
+func parseCliParams() Conf {
+	cmdParams := os.Args[1:]
+	config := Conf{
+		openPort: 6379,
+	}
+
+	for i, val := range cmdParams {
+		switch val {
+		case "--port", "-port", "--p", "-p":
+			if i+1 >= len(cmdParams) {
+				fmt.Println("Failed to parse parameter:", val)
+
+				os.Exit(1)
+			}
+
+			openPort, err := strconv.Atoi(cmdParams[i+1])
+
+			if err != nil {
+				fmt.Println("Failed to parse parameter:", val)
+
+				os.Exit(1)
+			}
+
+			config.openPort = openPort
+		case "--replicaof", "-replicaof", "--r", "-r":
+			if i+2 >= len(cmdParams) {
+				fmt.Println("Failed to parse parameter:", val)
+
+				os.Exit(1)
+			}
+
+			masterHost := cmdParams[i+1]
+			masterPort, err := strconv.Atoi(cmdParams[i+2])
+
+			if err != nil {
+				fmt.Println("Failed to parse parameter:", val)
+
+				os.Exit(1)
+			}
+
+			config.masterHost = masterHost
+			config.masterPort = masterPort
+		}
+	}
+
+	return config
+}
+
+func handleHandShake(config Conf) {
+	if config.masterHost == "" {
+		return
+	}
+
+	masterAddr := fmt.Sprintf("%s:%d", config.masterHost, config.masterPort)
+	connClient, err := net.Dial("tcp", masterAddr)
+
+	if err != nil {
+		fmt.Println("Dial failed:", err.Error())
+
+		os.Exit(1)
+	}
+
+	_, err = connClient.Write([]byte("*1\r\n$4\r\nping\r\n"))
+
+	if err != nil {
+		fmt.Println("Write to server failed:", err.Error())
+
+		os.Exit(1)
+	}
+
+	reply := make([]byte, 1024)
+
+	_, err = connClient.Read(reply)
+
+	if err != nil {
+		fmt.Println("Write to server failed:", err.Error())
+
+		os.Exit(1)
+	}
+
+	fmt.Println("reply from server=", string(reply))
+
+	err = connClient.Close()
+
+	if err != nil {
+		fmt.Println("Can't close the connection:", err.Error())
+
+		os.Exit(1)
 	}
 }
 
@@ -132,13 +217,13 @@ func HandleGetCommand(message []string, dict map[string]string) string {
 	return respMessage
 }
 
-func HandleInfoCommand(c Conf) string {
+func HandleInfoCommand(config Conf) string {
 	params := map[string]string{}
 
 	//role: Value is "master" if the instance is replica of no one, or "slave" if the instance is a replica of some master instance. Note that a replica can be master of another replica (chained replication).
 	params["role"] = "master"
 
-	if c.replicaOf != "" {
+	if config.masterHost != "" {
 		params["role"] = "slave"
 	}
 
